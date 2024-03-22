f460cc5239f8f80145d96d75f56e1ac5de473777
Kirera paul murithi <114237403+paul-murithi@users.noreply.github.com>
2024-03-22 21:02:43 +0300

    Update correction fix.

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the amenity class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-import models
+from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """Class Amenity
+    """This is the class for Amenity
     Attributes:
-             name (String): nme of amenity
+        name: input name
     """
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
-    # place_amenities = relationship('Place', secondary='place_amenity')
+    place_amenities = relationship("Place", secondary=place_amenity)
